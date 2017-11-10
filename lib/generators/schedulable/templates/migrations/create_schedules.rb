21e08b006dec5e2329bd15323fd3c016a340ef64
Ramses Lopez <ramses.lopezs@gmail.com>
2017-11-09 21:33:25 -0400

    updated migrations: occurrences now uses date & time data types, to reflect the datatypes of schedule

--- lib/generators/schedulable/templates/migrations/create_schedules.rb
+++ lib/generators/schedulable/templates/migrations/create_schedules.rb
@@ -2,22 +2,21 @@ class CreateSchedules < ActiveRecord::Migration[5.1]
   def self.up
     create_table :schedules do |t|
       t.references :schedulable, polymorphic: true
-      
+
       t.date :date
-      # t.time :time
-      
+
       t.time :start_time
       t.time :end_time
-      
+
       t.string :rule
       t.string :interval
-      
+
       t.text :day
       t.text :day_of_week
-      
+
       t.datetime :until
       t.integer :count
-      
+
       t.timestamps
     end
   end
